52a3c2e4edb39c9c03a1fbe8d30ea2b5a5039774
preetam verma <preetam.verma@hcl.com>
2023-02-16 13:33:32 +0530

    fix bugs

--- tests/integration/test_site.py
+++ tests/integration/test_site.py
@@ -1,9 +1,2 @@
-import pytest
-
-URL_test_success_data = "1"
-
-@pytest.mark.parametrize("URL, response", URL_test_success_data)
-def test_render_site_success(self, URL, response):
-    return True
-
-  
+def test_is_valid():
+   assert True
--- tests/unit/test_site.py
+++ tests/unit/test_site.py
@@ -1,8 +1,2 @@
-import pytest
-
-URL_test_data = [
-]
-
-@pytest.mark.parametrize("URL, response", URL_test_data)
-def test_is_valid(URL, response):
-   return True
+def test_is_valid():
+   assert True
